5212ef679872970e56b1fdb058f9fdc886590c3d
Huei Suen <46810130+heysuen@users.noreply.github.com>
2022-04-04 16:30:49 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { Heading, Text, Box, Image, Center } from '@chakra-ui/react';
+import jukebox from './assets/jukebox.gif';
+
+// SONG
+import track1 from './assets/heyma.mp3'
+
+
+
+function App() {
+  return (
+    <div className="main">
+      <Heading as='h1' className="text-space">HELLO</Heading>
+      <Text fontSize='lg' className="text-space">Welcome to the midnight jukebox!
+      </Text>
+      <Center>
+        <Image src={jukebox} boxSize='250px' >
+        </Image>
+      </Center>
+      <Center>
+        <audio src={track1} controls/>
+      </Center>
+      
+      
+    </div>    
+  );
+}
+
+export default App;
